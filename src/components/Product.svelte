ac898d337cf0a5263cdd8f0a1b9e3d0a24490ace
BluetabOS <oscar.bahamonde@bluetab.net>
2022-03-21 13:03:47 -0500

    k

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -3,7 +3,7 @@
     import { onMount } from "svelte";
 
     onMount(async()=>{
-        $products = await fetch("https://jsonplaceholder.typicode.com/photos").then(res => res.json())
+        $products = await fetch("https://my-json-server.typicode.com/oscarbahamonde/svelte-example/products").then(res => res.json())
     })
 
 </script>
@@ -11,16 +11,21 @@
 {#if $online}
 <div class="card lg:card-side bg-base-100 shadow-xl">
    
-        {#each $products as todo}
+        {#each $products as product}
          
         <div class="card-body">
-            <img src="{todo.url}"  alt="{todo.id}"/>
-            <h2 class="card-title">{todo.title}</h2>
-            <a href="{todo.url}">{todo.url}</a>
+        <div class="carousel carousel-vertical rounded-box">
+            {#each product.pictures as image}
+            <div class="carousel-item">
+            <img src="https://e-colibri.netlify.app/product/{product.id}/{image}.jpg"  alt="{image.id}"/>
+        </div>
+        {/each}
+    </div>
+            <h2 class="card-title">{product.name}</h2>
             <div class="card-actions justify-end">
               <button class="btn btn-primary flip-add"><i class="mdi mdi-cart text-3xl text-neutral" ></i></button> 
             </div>
-        </div>   
+            </div>  
             {/each}
      
 </div>
